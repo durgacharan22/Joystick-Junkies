7661795afbecf86b0a9f61264219636295ec6e93
Potukuru <s530748@ch112025m.nwmissouri.edu>
2018-02-16 11:47:17 -0600

    Linked signup and login pages

--- Joystick Junkies/ViewController.swift
+++ Joystick Junkies/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeview(_ sender: Any) {
+        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "tabviewcontroller")
+        let appdelegate =  UIApplication.shared.delegate as! AppDelegate
+        appdelegate.window?.rootViewController = tabBarController
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
