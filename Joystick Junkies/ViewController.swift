fd2e520d53786c0ba208e7b127e9992644a91762
Illendula <s530464@ch112025m.nwmissouri.edu>
2018-02-16 21:39:52 -0600

    linked login, signup, and home pages

--- Joystick Junkies/ViewController.swift
+++ Joystick Junkies/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     @IBAction func changeview(_ sender: Any) {
         let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "tabviewcontroller")
         let appdelegate =  UIApplication.shared.delegate as! AppDelegate
